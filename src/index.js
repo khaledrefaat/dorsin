606300b30153dfddadadb3ffbfc31666fac13f52
khaledrefaat <khaledrefaat08@gmail.com>
2021-07-15 06:11:51 +0200

    initial setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import reactDom from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import App from './App';
+
+reactDom.render(<App />, document.getElementById('root'));
