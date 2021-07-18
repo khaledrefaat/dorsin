318729dcd1015f6c799fe15762f6f950168df3d9
khaledrefaat <khaledrefaat08@gmail.com>
2021-07-18 19:24:28 +0200

    background done

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { header, header__layout, header__pattern } from "./Header.module.scss";
 
 const Header = () => {
-    return (
-        <div>
-            Header
-        </div>
-    )
-}
+  return (
+    <header
+      className={header}
+      style={{ backgroundImage: 'url("/images/bg-home.jpg")' }}
+    >
+      <div className={header__layout} />
+      <div className={header__pattern}>
+        <img src="/images/bg-pattern.png" alt="pattern" />
+      </div>
+      <div className="container">header</div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
