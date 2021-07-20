5eec2013759a57e15d8735b50c2e587f176c627b
khaledrefaat <khaledrefaat08@gmail.com>
2021-07-20 16:39:18 +0200

    section header completed

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { header, header__layout, header__pattern } from "./Header.module.scss";
 
 import Nav from "../components/Nav";
+import HeadingDescription from "../components/HeadingDescription";
 
 const Header = () => {
   return (
@@ -14,6 +15,10 @@ const Header = () => {
         <img src="/images/bg-pattern.png" alt="pattern" />
       </div>
       <Nav />
+      <HeadingDescription
+        headingContent="We help startups launch their products"
+        description="Etiam sed.Interdum consequat proin vestibulum class at a euismod mus luctus quam.Lorem ipsum dolor sit amet, consectetur adipisicing eli."
+      />
     </header>
   );
 };
