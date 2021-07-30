1046415f701cb6706c7c0602ce172887ba5c01ed
khaledrefaat <khaledrefaat08@gmail.com>
2021-07-30 23:46:15 +0200

    contact done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { btn, curved, white } from "./Button.module.scss";
 
-const Button = ({ children, iswhite, isCurved, styles }) => {
+const Button = ({ children, iswhite, isCurved, styles, ...rest }) => {
   return (
     <button
       className={`${btn} ${iswhite ? white : ""} ${isCurved ? curved : ""} ${
         styles ? styles : ""
       }`}
+      {...rest}
     >
       {children}
     </button>
--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
-import SectionHeader from "../components/SectionHeader";
+import SectionHeader from '../components/SectionHeader';
+import Button from '../components/Button';
 
 import {
   contact,
   contact__infoTitle,
   contact__infoMuted,
-} from "./Contact.module.scss";
+  firstInput,
+  error,
+} from './Contact.module.scss';
 
 const Contact = ({ sectionStyles }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <section className={`${sectionStyles} ${contact}`}>
       <div className="container">
@@ -45,18 +56,39 @@ const Contact = ({ sectionStyles }) => {
             </div>
           </div>
           <div className="col-lg-8 col-md-12">
-            <form action="#">
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="row mb-4 ">
-                <div className="col">
-                  <input type="text" placeholder="Your Name*" />
+                <div className={`col-lg-6 col-md-12 ${firstInput}`}>
+                  <input
+                    type="name"
+                    placeholder="Your Name*"
+                    {...register('name', { required: true })}
+                  />
+                  {errors?.name?.type === 'required' && (
+                    <p className={error}>This field is required</p>
+                  )}
                 </div>
-                <div className="col">
-                  <input type="email" placeholder="Your Email*" />
+                <div className="col-lg-6 col-md-12">
+                  <input
+                    type="email"
+                    placeholder="Your Emaill*"
+                    {...register('email', { required: true })}
+                  />
+                  {errors?.email?.type === 'required' && (
+                    <p className={error}>This field is required</p>
+                  )}
                 </div>
               </div>
               <div className="row mb-4">
                 <div className="col">
-                  <input type="text" placeholder="Your Subject.." />
+                  <input
+                    type="text"
+                    placeholder="Your Subject.."
+                    {...register('subject', { required: true })}
+                  />
+                  {errors?.subject?.type === 'required' && (
+                    <p className={error}>This field is required</p>
+                  )}
                 </div>
               </div>
               <div className="row">
@@ -66,6 +98,11 @@ const Contact = ({ sectionStyles }) => {
                     rows="4"
                     placeholder="Your Message.."
                   ></textarea>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col d-flex justify-content-end mt-3">
+                  <Button type="submit">Send Message</Button>
                 </div>
               </div>
             </form>
